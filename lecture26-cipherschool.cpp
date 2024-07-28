06033ddfd8c1c5b62257ecc9c81702d98974e1f7
Mohammad Roshan kk <118752658+MohammadRoshankk@users.noreply.github.com>
2024-07-28 13:38:11 +0530

    Create lecture26-cipherschool.cpp

---
+++ lecture26-cipherschool.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    Char j,o,h,n;
+    cin>>j>>o>>h>>n;
+    
+    char name[1000];
+    
+    cout<<"Enter your name : ( Note: Enter # after the end of your name: ) ";
+    
+    int i=0;  // Initial value of i is 0 
+    while(1){   // Here i is the previous 
+         cin>>name[i];
+        
+         if(name[i]=='#') break;
+         i++; // when name is begin entered, the value of i is incresing
+     }
+    
+     i=0; // Again need to change the value of i to 0 before running the second while loop
+     cout<<"The name you have just entered is "<<endl;
+     while(name[i]!='#'){
+         cout<<name[i];
+         i++;
+     }
