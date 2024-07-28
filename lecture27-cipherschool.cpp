e4da4ad95812c29da16b4b37e238fef7d68f4df9
Mohammad Roshan kk <118752658+MohammadRoshankk@users.noreply.github.com>
2024-07-28 13:48:43 +0530

    Create lecture27-cipherschool.cpp

---
+++ lecture27-cipherschool.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    
+    // string name;
+    // cin>>name;
+    // int i;
+    // for(i=0;i<5;i++){
+    //     cout<<name[i]<<endl;
+    // }
+    
+    
+    
+    
+    
+    // string a;
+    // string b;
+    // cin>>a>>b;
+    // cout<<"The strings which you have given as input are "<<a<<" and "<<b<<endl;
+    
+    // string c;
+    // c= a+b; // This is called concatination
+    // cout<<"The combined word is: "<<c<<endl;
+    // cout<<"The length of the combined word is : "<<c.length()<<endl;
+    // return 0;
+    
+    
